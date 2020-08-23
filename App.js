df1859a148f591e679f50c0af6d1975ba55665c0
turboguy <turboguy36@gmail.com>
2020-08-23 23:55:23 +0900

    git test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World Friends</Text>
+      <Text>Hello World Friends!</Text>
       <StatusBar style="auto" />
     </View>
   );
